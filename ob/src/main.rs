712115a46e86b5cf35f671693d3b4aebf4ffb3bf
LighghtEeloo <litiaeeloo@gmail.com>
2023-08-17 16:46:54 +0800

    incr: macos

--- ob/src/main.rs
+++ ob/src/main.rs
@@ -1,7 +1,13 @@
-use tray_item::{TrayItem, IconSource};
+use tray_item::{IconSource, TrayItem};
 
 fn main() -> anyhow::Result<()> {
     let mut tray = TrayItem::new("mio-ring", IconSource::Resource("tray-icon-in-rc-file"))?;
     tray.add_label("mio-ring")?;
-    Ok(())
+    #[cfg(target_os = "macos")]
+    {
+        let mut inner = tray.inner_mut();
+        inner.add_quit_item("Quit");
+        inner.display();
+    }
+    loop {}
 }
